0b00d2677ee17527bfa439fd11ba08540236b5cd
Bioshaun <ricekent@163.com>
2019-07-13 05:31:25 -0400

    add async_batch_sh_jobs

--- omutils/_shell.py
+++ omutils/_shell.py
@@ -1,11 +1,19 @@
 import asyncio
 
 
-async def async_run(cmd, semaphore):
-    with await semaphore:
-        proc = await asyncio.create_subprocess_shell(
+async def async_sh_job(cmd, sema):
+    with (await sema):
+        p = await asyncio.create_subprocess_shell(
             cmd,
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
-        stdout, stderr = await proc.communicate()
-    return stdout, stderr
+            stderr=asyncio.subprocess.STDOUT)
+        return (await p.communicate())[0].splitlines()
+
+
+def async_batch_sh_jobs(cmd_list, thread=2):
+    semaphore = asyncio.Semaphore(thread)
+    loop = asyncio.get_event_loop()
+    if cmd_list:
+        coro_list = [async_sh_job(cmd, semaphore) for cmd in cmd_list]
+        loop.run_until_complete(asyncio.wait(coro_list))
+    loop.close()
